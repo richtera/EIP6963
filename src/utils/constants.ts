1e4d17d226161247102a8651121415c0e248dbde
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-04-11 06:28:49 -0400

    Update constants.ts
    Add Universal Profiles.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -111,4 +111,8 @@ export const supportedWallets = [
     name: "Loopring",
     url: "https://www.loopring.io",
   },
+  {
+    name: "Universal Profiles",
+    url: "https://my.universalprofile.cloud",
+  },
 ];
